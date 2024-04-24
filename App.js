4b0cfd7dea6aa636f4e0de2ed01d5094270fbec5
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-24 12:46:18 +0900

    2.5 Styles

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Text, } from "react-native";
 
 export default function App() {
   return(
-    <View style={{ flex: 1, flexDirection: "row"}}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 2, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
-      {/* <View style={{ flex: 1, backgroundColor: "teal" }}></View> */}
-    </View>
-    
+    <View style={styles.container}>
+      <StatusBar style="light"/>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </View>
+    </View>  
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1, backgroundColor: "tomato"
+  },
+
+  city: {
+    flex: 1.2,
+    // backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+
+  weather: {
+    flex: 3,
+    // backgroundColor: "red"
+  },
+
+  day: {
+    flex: 1,
+    // justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "teal",
+  },
+
+  temp: {
+    fontSize: 170,
+    marginTop: 50,
+
+  },
+
+  desc: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+})
