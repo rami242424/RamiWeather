65c750ef6cac36265a40027f93b9399ed811a47b
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-24 12:49:29 +0900

    #2.6 Styles part Two - ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, StyleSheet, Text, } from "react-native";
+import { View, StyleSheet, Text, ScrollView, } from "react-native";
 
 export default function App() {
   return(
@@ -9,12 +9,20 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView style={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.desc}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>  
   );
 }
