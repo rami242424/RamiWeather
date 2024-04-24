0b610fb05f2ef0ab03b3fb9a70bea07e4c28051c
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-24 12:17:05 +0900

    2.4 Layout System -2 : 부모의 flex 중요 / defalut direction 은 coloum

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { View } from "react-native";
 
 export default function App() {
   return(
-    <View>
-      <View style={{ width: 200, height: 200, backgroundColor: "tomato"}}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "green"}}></View>
-      <View style={{ width: 200, height: 200, backgroundColor: "orange"}}></View>
+    <View style={{ flex: 1, flexDirection: "row"}}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 2, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+      {/* <View style={{ flex: 1, backgroundColor: "teal" }}></View> */}
     </View>
+    
   );
 }
