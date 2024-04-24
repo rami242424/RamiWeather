15dde731a49c780cb5090b4e3b97fde3f5519734
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-24 12:03:30 +0900

    2.4 Layout System -1

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!!!where is error</Text>
-      <StatusBar style="auto" />
+  return(
+    <View>
+      <View style={{ width: 200, height: 200, backgroundColor: "tomato"}}></View>
+      <View style={{ width: 200, height: 200, backgroundColor: "green"}}></View>
+      <View style={{ width: 200, height: 200, backgroundColor: "orange"}}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-  }
-
-});
